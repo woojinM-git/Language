c53304e897e9b2391523b70a2d5f1b759374b14d
mwj02 <145192679+woojinM-git@users.noreply.github.com>
2025-04-22 20:56:50 +0900

    JS 기초를 공부했습니다

---
+++ JS/7_operators.js
@@ -0,0 +1,39 @@
+/**
+ * Operators
+ * 
+ * 연산자
+ */
+
+/**
+ * 산술 연산자
+ * 
+ * 1) 덧셈
+ * 2) 뺄셈
+ * 3) 곱셈
+ * 4) 나눗셈
+ * 5) 나머지
+ */
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 3);
+
+console.log('--------------------');
+
+console.log(10 * (10 + 10));
+
+/**
+ * 증가와 감소
+ */
+let number = 1;
+
+number ++;
+console.log(number);
+
+number --;
+console.log(number);
+
+/**
+ * 연산자의 위치
+ */
